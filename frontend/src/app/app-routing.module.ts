1c33b6f84db2f7f67ba1f67877bb9b05dd2fdbe1
Joseph <josephsantiago2345@gmail.com>
2026-06-27 12:08:58 -0500

    feat(routing): creadno el enrutamiento principal

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './features/landing/landing.component';
+import { PrimerosPasosComponent } from './features/primeros-pasos/primeros-pasos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'primeros-pasos', component: PrimerosPasosComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', anchorScrolling: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
