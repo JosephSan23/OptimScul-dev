e489b438fa8a7548a365fbe088dc32534b1f7a64
Joseph <josephsantiago2345@gmail.com>
2026-06-28 13:52:57 -0500

    feat: landing page, navbar y footer en Angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { LandingComponent } from './features/landing/landing.component';
+import { PrimerosPasosComponent } from './features/primeros-pasos/primeros-pasos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+    PrimerosPasosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/src/app/features/primeros-pasos/primeros-pasos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeros-pasos',
+  templateUrl: './primeros-pasos.component.html',
+  styleUrl: './primeros-pasos.component.scss'
+})
+export class PrimerosPasosComponent {
+
+}
