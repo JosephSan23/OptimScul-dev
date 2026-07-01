36d88b750ec682b9e954f2bdc819c8e257e82549
Joseph <josephsantiago2345@gmail.com>
2026-07-01 13:22:13 -0500

    feat(repository): Creando el UsuarioRepositoryAdapter

---
+++ backend/src/main/java/backend/security/infrastructure/persistence/adapter/UsuarioRepositoryAdapter.java
@@ -0,0 +1,52 @@
+package backend.security.infrastructure.persistence.adapter;
+
+import backend.security.application.port.UsuarioRepository;
+import backend.security.domain.model.Usuario;
+import backend.security.infrastructure.persistence.UsuarioJpaRepository;
+import backend.security.infrastructure.persistence.mapper.UsuarioMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class UsuarioRepositoryAdapter implements UsuarioRepository {
+
+    private final UsuarioJpaRepository jpaRepository;
+    private final UsuarioMapper mapper;
+
+    public UsuarioRepositoryAdapter(UsuarioJpaRepository jpaRepository, UsuarioMapper mapper) {
+        this.jpaRepository = jpaRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Usuario save(Usuario usuario) {
+        return mapper.toDomain(jpaRepository.save(mapper.toEntity(usuario)));
+    }
+
+    @Override
+    public Optional<Usuario> findById(UUID id) {
+        return jpaRepository.findById(id).map(mapper::toDomain);
+    }
+
+    @Override
+    public List<Usuario> findAll() {
+        return jpaRepository.findAll()
+                .stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        jpaRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Usuario> findByUsernameOrEmail(String valor) {
+        return jpaRepository.findByUsernameOrCorreo(valor).map(mapper::toDomain);
+    }
+}
