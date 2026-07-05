db43a6e6b8bcab900946ddea20db569e26eda600
Joseph <josephsantiago2345@gmail.com>
2026-07-05 12:31:55 -0500

    Añadiendo el metodo si existe por numero de documento a las operaciones jpa

--- backend/src/main/java/backend/people/infrastructure/persistence/PersonaJpaRepository.java
+++ backend/src/main/java/backend/people/infrastructure/persistence/PersonaJpaRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PersonaJpaRepository extends JpaRepository<PersonaEntity, UUID> {
+    boolean existsByNumeroDocumento(String numeroDocumento);
 }
