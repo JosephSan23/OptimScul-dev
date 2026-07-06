d0d88d1df59dabbc15d4e13f1e200ac957032173
Joseph <josephsantiago2345@gmail.com>
2026-07-06 15:07:57 -0500

    Añadiendo los metodos jpa para encontrar por usuario e institucion

--- backend/src/main/java/backend/security/infrastructure/persistence/UsuarioInstitucionJpaRepository.java
+++ backend/src/main/java/backend/security/infrastructure/persistence/UsuarioInstitucionJpaRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UsuarioInstitucionJpaRepository extends JpaRepository<UsuarioInstitucionEntity, UUID> {
+    java.util.List<UsuarioInstitucionEntity> findByUsuarioId(UUID usuarioId);
+    java.util.List<UsuarioInstitucionEntity> findByInstitucionId(UUID institucionId);
 }
