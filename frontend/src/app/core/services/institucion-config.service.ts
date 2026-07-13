1a35eafbeaabc766548eb673a8e1321c421f31f0
Joseph <josephsantiago2345@gmail.com>
2026-07-13 12:09:28 -0500

    feat: Creando el institucion-config service y añadiendolo al layout

---
+++ frontend/src/app/core/services/institucion-config.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface InstitucionConfig {
+  codigo: string;
+  tipoInstitucion: string;
+  estado: string;
+  dominioCorreo?: string;
+  nombre: string;
+  nombreCorto?: string;
+  descripcion?: string;
+  nit?: string;
+  dane?: string;
+  resolucionFuncionamiento?: string;
+  correoContacto?: string;
+  telefonoContacto?: string;
+  sitioWeb?: string;
+  direccionPrincipal?: string;
+  ciudad?: string;
+  departamento?: string;
+  pais?: string;
+  zonaHoraria?: string;
+  moneda?: string;
+}
+export interface InstitucionConfigRequest {
+  nombre: string;
+  nombreCorto?: string;
+  descripcion?: string;
+  nit?: string;
+  dane?: string;
+  resolucionFuncionamiento?: string;
+  correoContacto?: string;
+  telefonoContacto?: string;
+  sitioWeb?: string;
+  direccionPrincipal?: string;
+  ciudad?: string;
+  departamento?: string;
+  pais?: string;
+  zonaHoraria?: string;
+  moneda?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class InstitucionConfigService {
+  private readonly API = 'http://localhost:8080/api/config/institucion';
+  constructor(private http: HttpClient) {}
+
+  miInstitucion(): Observable<InstitucionConfig> {
+    return this.http.get<InstitucionConfig>(this.API);
+  }
+  guardar(datos: InstitucionConfigRequest): Observable<InstitucionConfig> {
+    return this.http.put<InstitucionConfig>(this.API, datos);
+  }
+}
