3332181c3a3a6f434bccdea2971383372a5e23a5
Joseph <josephsantiago2345@gmail.com>
2026-07-13 20:12:22 -0500

    feaT: Creando el periodoService en el front

---
+++ frontend/src/app/core/services/periodo.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Periodo {
+  id: string;
+  anioLectivoId: string;
+  numero: number;
+  nombre: string;
+  descripcion?: string;
+  fechaInicio?: string;
+  fechaFin?: string;
+  peso?: number;
+  estado: string;
+}
+export interface PeriodoRequest {
+  numero: number | null;
+  nombre: string;
+  descripcion?: string;
+  fechaInicio?: string;
+  fechaFin?: string;
+  peso?: number | null;
+  estado?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class PeriodoService {
+  private readonly API = 'http://localhost:8080/api/config/periodos';
+  constructor(private http: HttpClient) {}
+  listarPorAnio(anioId: string): Observable<Periodo[]> {
+    return this.http.get<Periodo[]>(`${this.API}/por-anio/${anioId}`);
+  }
+  obtener(id: string): Observable<Periodo> {
+    return this.http.get<Periodo>(`${this.API}/${id}`);
+  }
+  crear(anioId: string, d: PeriodoRequest): Observable<Periodo> {
+    return this.http.post<Periodo>(`${this.API}/por-anio/${anioId}`, d);
+  }
+  editar(id: string, d: PeriodoRequest): Observable<Periodo> {
+    return this.http.put<Periodo>(`${this.API}/${id}`, d);
+  }
+}
